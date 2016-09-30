561ba517369d9f0c09028138841e1b967a5b2956
Ben Christianson <thhuntertgm@gmail.com>
2016-09-30 11:43:23 -0500

    Improved error handling, split out realm_status

--- lib/api.ex
+++ lib/api.ex
@@ -1,10 +1,18 @@
 defmodule Flask.API do
   use HTTPoison.Base
 
-  def fetch!(endpoint, queries \\ %{}) do
+  def fetch(endpoint, queries \\ %{}) do
     q = Map.merge(queries, %{apikey: Flask.Config.api_key})
     url = "#{Flask.Config.api_url}#{endpoint}?#{URI.encode_query(q)}"
-    get!(url) |> Map.get(:body)
+    body = Map.get(get!(url), :body)
+
+    # Handle errors gracefully!
+    case body do
+      [code: code, detail: detail, type: type] ->
+        {:error, "#{code} #{type} (#{detail})"}
+      body ->
+        {:ok, body}
+    end
   end
 
   def process_response_body(body) do
--- lib/wow.ex
+++ lib/wow.ex
@@ -1,6 +1,10 @@
 defmodule Flask.WoW do
-  def realms(realms \\ []) do
+  def realms do
+    Flask.API.fetch("/wow/realm/status")
+  end
+
+  def realm_status(realms) when is_list(realms) do
     queries = %{realms: Enum.join(realms, ",")}
-    Flask.API.fetch!("/wow/realm/status", queries)
+    Flask.API.fetch("/wow/realm/status", queries)
   end
 end
